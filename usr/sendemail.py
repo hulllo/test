559a9ad9367f7847df12c929205b8c302573babd
hulllo <hulllo@outlook.com>
2017-11-03 22:05:22 +0800

    errrrrr

--- usr/sendemail.py
+++ usr/sendemail.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-' a smtp send email module '
-' 参数1：邮件标题 '
-' 参数2：邮件内容 '
+
 
 
 __author__ = 'yuchuang.pan'
@@ -21,16 +19,19 @@ def _format_addr(s):
     name, addr = parseaddr(s)
     return formataddr((Header(name, 'utf-8').encode(), addr))
 def send_(emailsubject,emailcontent):
+        ' a smtp send email module '
+        ' emailsubject：邮件标题 '
+        ' emailcontent：邮件内容 '
 	# 输入Email地址和口令:
         from_addr = input('From: ')
         password = input('Password: ')
-	# from_addr = input('输入用来发送邮件的邮箱地址（只支持outlook邮箱）'
-	# password = input('输入邮箱密码')
-	# 输入收件人地址:
+    # from_addr = input('输入用来发送邮件的邮箱地址（只支持outlook邮箱）'
+    # password = input('输入邮箱密码')
+    # 输入收件人地址:
         to_addr = input('To: ')
-	#to_addr = 'hulllo@outlook.com'
-	# 输入SMTP服务器地址:
-	# smtp_server = input('SMTP server: ')
+    #to_addr = 'hulllo@outlook.com'
+    # 输入SMTP服务器地址:
+    # smtp_server = input('SMTP server: ')
         smtp_server = 'smtp-mail.outlook.com'
 
         msg = MIMEText(emailcontent, 'plain', 'utf-8')
@@ -38,7 +39,7 @@ def send_(emailsubject,emailcontent):
         msg['To'] = _format_addr('管理员 <%s>' % to_addr)
         msg['Subject'] = Header(emailsubject, 'utf-8').encode()
 
-	# smtp.starttls()
+    # smtp.starttls()
         server = smtplib.SMTP(smtp_server, 587) # SMTP协议默认端口是25
         server.starttls()
         server.set_debuglevel(1)
