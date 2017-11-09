a89fa019a79b997e830d87fb723c572edcbdd596
hulllo <hulllo@outlook.com>
2017-11-09 17:21:36 +0800

    1.取消改变默认编码输出；2.发email格式更改为html

--- moviepacong.py
+++ moviepacong.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import sys  
+import sys
 import usr.sendemail
 import io
 try:
@@ -16,7 +16,7 @@ movie = []
 pingfeng = []
 links = []
 str = ''
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8') #改变标准输出的默认编码  
+# sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8') #改变标准输出的默认编码
 response = requests.get("http://gaoqing.fm/")
 soup = BeautifulSoup(response.content,"html.parser")
 print("获取主页面完成")
@@ -29,10 +29,10 @@ for tag1 in tag.find_all('a',target="_blank")[4:100:2]:
 	# print(tag1.string)
 for tag2 in soup.find_all('span',style="color:#CF0000"):
 	pingfeng.append(tag2.string)
-# n = len(links)	
+# n = len(links)
 # print(n)
 print("获取电影列表完成")
-# print(movie)
+print(movie)
 # print(pingfeng)
 # print(links)
 for n in range(len(links)):
@@ -42,26 +42,26 @@ for n in range(len(links)):
 	# print(soup)
 	tag =  soup.find_all('div',class_="col-md-9")[0]
 	temp = tag.find_all('a')[5].string
-	print("获取导演",n+1)
+	print("获取导演",n+1,temp)
 	directors.append(temp)
 # print(directors)
 
 n =len(movie)
 
 for x in range(len(movie)):
+	if float(pingfeng[x]) >= 7:
+		str+= '<html><body><a href="'+links[x]+'" target="_blank">'+movie[x]+'\t'+'<font color="#FF0000">'+pingfeng[x]+'</font>'+'\t'+directors[x]+'</a></body></html><br />'
+	else:
+		str+= '<html><body><a href="'+links[x]+'" target="_blank">'+movie[x]+'\t'+'<font color="#00FF00">'+pingfeng[x]+'</font>'+'\t'+directors[x]+'</a></body></html><br />'
 
-	str+= '<html><body><a href="'+links[x]+'" target="_blank">'+movie[x]+'\t'+pingfeng[x]+'\t'+directors[x]+'</a></body></html>'+'\n'
-
-	
-with open('movie_data.txt','r') as movie_data:
+with open('movie_data.txt','r',encoding = 'utf-8') as movie_data:
 	data = movie_data.read()
 
 	if data == str:
-		print('movie no updata') 
+		print('movie no updata')
 	else:
-		with open('movie_data.txt','w') as movie_data:
-			movie_data.write(str)	
-			usr.sendemail.send_('电影更新','以下是最近更新的电影目录：\n'+str)
-		print('send email ok')	
+		with open('movie_data.txt','w',encoding = 'utf-8') as movie_data:
+			movie_data.write(str)
+			usr.sendemail.send_('电影更新','以下是最近更新的电影目录：<br />'+str)
+		print('send email ok')
 
-  
