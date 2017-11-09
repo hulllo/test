e36e3aba5f5c8f6bc358eb3aca08ef5821f84a95
hulllo <hulllo@outlook.com>
2017-11-09 17:22:19 +0800

    取消改变默认编码输出

--- remendianying.py
+++ remendianying.py
@@ -4,8 +4,8 @@ import logging
 import os
 import re
 import time
-import io  
-import sys  
+import io
+import sys
 try:
     from urllib.parse import urlparse  # py3
 except:
@@ -16,23 +16,22 @@ import requests
 from bs4 import BeautifulSoup
 
 list = []
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8') #改变标准输出的默认编码  
+# sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8') #改变标准输出的默认编码
 response = requests.get("http://gaoqing.fm/")
 soup = BeautifulSoup(response.content,"html.parser")
 # for string in soup.find_all(class_='tr01')[3].stripped_strings:
     # print(repr(string))
 tag = soup.find_all('div',style="padding:5px;padding-bottom:10px;border-bottom:2px #ccc dotted")
 for tag1 in tag[1].find_all('div',style="position:relative;float:left"):
-    
+
     for y in tag1.stripped_strings:
         list.append(y)
         # print(y)
 # for x in tag1.stripped_strings:
     # print(x)
-    
+
 for tag2 in tag[1].find_all('a',target="_blank",style="font-size:12px"):
     list.append(tag2.string)
     # print(tag2.string)
-print (list)   
+print (list)
 input()
-  
